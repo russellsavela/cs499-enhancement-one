89df31422490288eb0ca25ebf6ea582a73d662ff
Russ Savela <russell.savela@snhu.edu>
2025-07-20 01:26:16 -0400

    fixed another typo

--- terraform/enhancement.one.tf
+++ terraform/enhancement.one.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "snhu_dynamodb" {
 EOF
 }
 
-resouce "aws_iam_role_policy_attachment" "enhancement_one_attachment" {
+resource "aws_iam_role_policy_attachment" "enhancement_one_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
   role = aws_iam_role.nhu_dynamodb
 
