d14ee00ca7c13b0e2b7b764e5ad54b07d4404b6e
Russ Savela <russell.savela@snhu.edu>
2025-07-20 00:53:37 -0400

    clone code from git

--- terraform/user-data/config-deps.sh
+++ terraform/user-data/config-deps.sh
@@ -9,7 +9,12 @@ pip3 install dash_leaflet dash pandas matplotlib boto3
 
 
 # git clone the code we are going to run
-#
+#  -it is public, so no keys needed
+
+git clone https://github.com/russellsavela/cs499-enhancement-one.git
  
  
 # Run the app
+#
+
+python3 cs499-enhancement-one/enhancement.one.py
