27dab7d9368d8ac294b9f5a800a5f6aa5365e9c9
Russ Savela <russell.savela@snhu.edu>
2025-07-20 01:23:43 -0400

    added policy attachment

--- terraform/enhancement.one.tf
+++ terraform/enhancement.one.tf
@@ -20,6 +20,13 @@ resource "aws_iam_role" "snhu_dynamodb" {
 EOF
 }
 
+resouce "aws_iam_role_policy_attachment" "enhancement_one_attachment" {
+  policy_arn = ""arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+  role = aws_iam_role.nhu_dynamodb
+
+}
+
+
 resource "aws_iam_instance_profile" "dynamodb_access" {
   name = "dynamodb-instance-profile"
   role = aws_iam_role.snhu_dynamodb
